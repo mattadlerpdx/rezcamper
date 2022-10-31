11083caf94bc0aa0dde3a6156b0bbd93096eea5b
Kira <kirak@pdx.edu>
2022-10-31 11:45:53 -0700

    Removed class definition in validateEmail.py, renamed files, added 9 tests to unittests in testValidateEmail.py

---
+++ src/validateEmail.py
@@ -0,0 +1,15 @@
+#Implementationa of email verification using the email_validator library
+#Citation: pypi.org/project/email-validator/
+
+from email_validator import validate_email, EmailNotValidError
+
+
+def validateEmail(emailReceived):
+    try:
+        validatedEmail = validate_email(emailReceived)
+        return str(validatedEmail.ascii_email)
+    except EmailNotValidError as error:
+        print(str(error))
+    
+
+
