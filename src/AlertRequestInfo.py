2e17d3124db17165bdfb2573b1be03cf390639bc
Kira <kirak@pdx.edu>
2022-10-31 12:44:57 -0700

    Began building out the Post Processing class and the Altert Info class. Alert Info has no methods at present, and just hold the validated data to be sent to the next stage of the system. We can build this out further later.

---
+++ src/AlertRequestInfo.py
@@ -0,0 +1,11 @@
+from validateEmail import *
+
+class AlertRequestInfo:
+    def __init__(self, email, campsite, dates):
+        self.validEmail = email
+        self.validCampsite = campsite 
+        self.validDates = dates
+
+ 
+        
+
