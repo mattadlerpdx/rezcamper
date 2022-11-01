9799952880f0cecedeb677d3c98efc871c3a51fb
Tito Reinhart <TKral@pdx.edu>
2022-10-31 20:28:36 -0700

    fix date testing

--- tests/testValidateDate.py
+++ tests/testValidateDate.py
@@ -6,17 +6,17 @@ from src.validateDate import validateDate
 class TestValidateDate(unittest.TestCase):
 
     def testTypeError(self):
-        self.assertRaises(TypeError, validateDate.validateDate, None)
-        self.assertRaises(TypeError, validateDate.validateDate, {})
-        self.assertRaises(TypeError, validateDate.validateDate, False)
-        self.assertRaises(TypeError, validateDate.validateDate, [1, 2])
+        self.assertRaises(TypeError, validateDate, None)
+        self.assertRaises(TypeError, validateDate, {})
+        self.assertRaises(TypeError, validateDate, False)
+        self.assertRaises(TypeError, validateDate, [1, 2])
 
     def testTrue(self):
-        self.assertTrue(validateDate.validateDate("1994-11-05"))
+        self.assertTrue(validateDate("1994-11-05"))
 
     def testValueError(self):
-        self.assertRaises(ValueError, validateDate.validateDate, "1994/11/05")
-        self.assertRaises(ValueError, validateDate.validateDate, "1994 11 05")
+        self.assertRaises(ValueError, validateDate, "1994/11/05")
+        self.assertRaises(ValueError, validateDate, "1994 11 05")
 
 
 if __name__ == '__main__':
