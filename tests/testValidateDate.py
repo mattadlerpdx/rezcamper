9c41e5023687480b713157665ce7258002ad9969
matt <madler@pdx.edu>
2022-10-31 18:08:35 -0700

    can import validateDate

---
+++ tests/testValidateDate.py
@@ -0,0 +1,24 @@
+
+import unittest
+from src.validateDate import validateDate
+
+
+class TestValidateDate(unittest.TestCase):
+
+    def testTypeError(self):
+        self.assertRaises(TypeError, validateDate, None)
+        self.assertRaises(TypeError, validateDate, {})
+        self.assertRaises(TypeError, validateDate, False)
+        self.assertRaises(TypeError, validateDate, [1, 2])
+
+    def testTrue(self):
+        self.assertTrue(validateDate("1994-11-05"))
+
+    def testValueError(self):
+        self.assertRaises(ValueError, validateDate, "1994/11/05")
+        self.assertRaises(ValueError, validateDate, "1994 11 05")
+
+
+if __name__ == '__main__':
+
+    unittest.main()
