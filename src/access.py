7bf75ab8075502d64722d0ddafd0bdfceb4ab789
Tito Reinhart <TKral@pdx.edu>
2022-11-15 13:16:17 -0800

    hackey access to api

--- src/access.py
+++ src/access.py
@@ -4,8 +4,30 @@ import urllib.request
 import json
 
 api_key = os.environ.get("NP_API_KEY")
-base_url = f'developer.nps.gov/api/v1'
-endpoint = "https://developer.nps.gov/api/v1/activities/parks?stateCode=or"
-HEADERS = {"Authorization":f"{api_key}"}
+'''
+import urllib.request, json
+# Configure API request
+endpoint = "https://developer.nps.gov/api/v1/parks?stateCode=me"
+HEADERS = {"Authorization":str(api_key)}
 req = urllib.request.Request(endpoint,headers=HEADERS)
 
+# Execute request and parse response
+response = urllib.request.urlopen(req).read()
+data = json.loads(response.decode('utf-8'))
+# Additional code would follow
+'''
+
+import urllib.request, json
+# Configure API request
+endpoint = "https://developer.nps.gov/api/v1/parks?stateCode=me"
+api_key = os.environ.get('NP_API_KEY')
+pass_key = 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=fqVm0mEF5FCSuXrgskibHGKmretdviKbvQNMdmNG'
+HEADERS = {"Authorization":"fqVm0mEF5FCSuXrgskibHGKmretdviKbvQNMdmNG"}
+req = urllib.request.Request(endpoint,headers=HEADERS)
+# Additional code would follow
+
+response = urllib.request.urlopen(pass_key).read()
+data = json.loads(response.decode('utf-8'))
+
+# Prepare and execute output
+print(data["data"][0]["fullName"] + " can be found at " + data["data"][0]["latLong"] + ".")
