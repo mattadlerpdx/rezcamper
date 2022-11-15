2524d09b9dd6fd4bb0e54f51567e5d07cd76dd81
Tito Reinhart <TKral@pdx.edu>
2022-11-15 13:20:19 -0800

    starting to figure out shitty api

--- src/access.py
+++ src/access.py
@@ -21,13 +21,13 @@ import urllib.request, json
 # Configure API request
 endpoint = "https://developer.nps.gov/api/v1/parks?stateCode=me"
 api_key = os.environ.get('NP_API_KEY')
-pass_key = f'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key={api_key}'
+pass_key = f'https://developer.nps.gov/api/v1/parks?parkCode=*&api_key={api_key}'
 HEADERS = {"Authorization":f"{api_key}"}
 req = urllib.request.Request(endpoint,headers=HEADERS)
 # Additional code would follow
 
 response = urllib.request.urlopen(pass_key).read()
 data = json.loads(response.decode('utf-8'))
-
+print(data)
 # Prepare and execute output
-print(data["data"][0]["fullName"] + " can be found at " + data["data"][0]["latLong"] + ".")
+#print(data["data"][0]["fullName"] + " can be found at " + data["data"][0]["latLong"] + ".")
