1184669c46ecaa40c04d21dbeec3d536fd616a3b
Tito Reinhart <TKral@pdx.edu>
2022-11-15 13:33:14 -0800

    gathering information

--- src/access.py
+++ src/access.py
@@ -21,13 +21,20 @@ import urllib.request, json
 # Configure API request
 endpoint = "https://developer.nps.gov/api/v1/parks?stateCode=me"
 api_key = os.environ.get('NP_API_KEY')
-pass_key = f'https://developer.nps.gov/api/v1/parks?parkCode=*&api_key={api_key}'
+pass_key = f'https://developer.nps.gov/api/v1/activities?parkCode=or&api_key={api_key}'
 HEADERS = {"Authorization":f"{api_key}"}
 req = urllib.request.Request(endpoint,headers=HEADERS)
 # Additional code would follow
 
 response = urllib.request.urlopen(pass_key).read()
 data = json.loads(response.decode('utf-8'))
-print(data)
+#print(data['total'])
+#print(data['limit'])
+#print(data['start'])
+with open('data_output.json', 'w') as fp:
+    for line in data['data']:
+        fp.write(str(line))
+#for k in data:
+#    data[k]
 # Prepare and execute output
 #print(data["data"][0]["fullName"] + " can be found at " + data["data"][0]["latLong"] + ".")
