82933d031bb5ad18af29be42c72368954f621a6c
Tito Reinhart <TKral@pdx.edu>
2022-11-15 15:12:39 -0800

    added api data structure

--- src/access.py
+++ src/access.py
@@ -16,6 +16,38 @@ JSON_KEYS = [
     'data',
 ]
 
+API_METHODS = [
+    'activities',
+    'activities/parks',
+    'alerts',
+    'amenities',
+    'amenities/parksplaces',
+    'amenities/parksvisitorcenters',
+    'articles',
+    'campgrounds',
+    'events',
+    'lessonplans',
+    'multimedia/audio',
+    'multimedia/videos',
+    'newsreleases',
+    'parkinglots',
+    'parks',
+    'passportstamplocations',
+    'people',
+    'places',
+    'thingstodo',
+    'topics',
+    'topics/parks',
+    'tours',
+    'visitorcenters',
+    'webcams',
+]
+#TODO:
+'''
+Construct a file output constructor to seperate
+API Data and information.
+'''
 with open('data_output.json', 'w') as fp:
-    for line in data['data']:
-        fp.write(json.dumps(line) + '\n')
+    for key in JSON_KEYS:
+        for line in data[key]:
+            fp.write(json.dumps(line) + '\n')
