01e293a476c24886c527482b004314877802ec03
Tito Reinhart <TKral@pdx.edu>
2022-11-15 14:16:32 -0800

    shit

--- src/access.py
+++ src/access.py
@@ -6,7 +6,6 @@ api_key = os.environ.get("NP_API_KEY")
 # Configure API request
 endpoint = "https://developer.nps.gov/api/v1/parks?stateCode=me"
 api_key = os.environ.get('NP_API_KEY')
-api_key2 =  'y3v7P3EIl474SZMn9SbwctBjguUqhhIg3NbINM2b' 
 pass_key = f'https://developer.nps.gov/api/v1/campgrounds?parkCode=or&api_key={api_key}'
 
 response = urllib.request.urlopen(pass_key).read()
