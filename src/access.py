29d05af6ee471e7aa90f681e1ad191e322107f81
Tito Reinhart <TKral@pdx.edu>
2022-11-15 12:52:34 -0800

    working and figuring out api

--- src/access.py
+++ src/access.py
@@ -1,9 +1,11 @@
 import os
 import subprocess
-import requests
 import urllib.request
 import json
 
 api_key = os.environ.get("NP_API_KEY")
-endpoint = "https://developer.nps.gov/api/v1/parks?stateCode=me"
+base_url = f'developer.nps.gov/api/v1'
+endpoint = "https://developer.nps.gov/api/v1/activities/parks?stateCode=or"
 HEADERS = {"Authorization":f"{api_key}"}
+req = urllib.request.Request(endpoint,headers=HEADERS)
+
