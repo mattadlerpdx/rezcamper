df933ffb0ff9733c2e76082bcf5be32671041e0d
Tito Reinhart <TKral@pdx.edu>
2022-11-15 14:15:28 -0800

    cleanup access to api

--- src/access.py
+++ src/access.py
@@ -6,17 +6,19 @@ api_key = os.environ.get("NP_API_KEY")
 # Configure API request
 endpoint = "https://developer.nps.gov/api/v1/parks?stateCode=me"
 api_key = os.environ.get('NP_API_KEY')
+api_key2 =  'y3v7P3EIl474SZMn9SbwctBjguUqhhIg3NbINM2b' 
 pass_key = f'https://developer.nps.gov/api/v1/campgrounds?parkCode=or&api_key={api_key}'
-HEADERS = {"Authorization":f"{api_key}"}
-req = urllib.request.Request(endpoint,headers=HEADERS)
-# Additional code would follow
 
 response = urllib.request.urlopen(pass_key).read()
 data = json.loads(response.decode('utf-8'))
-#print(data['total'])
-#print(data['limit'])
-#print(data['start'])
-count = 0
+
+JSON_KEYS = [
+    'total',
+    'limit',
+    'start',
+    'data',
+]
+
 with open('data_output.json', 'w') as fp:
     for line in data['data']:
         fp.write(json.dumps(line) + '\n')
