797a55354e974787781c2c4bfb394509264a4103
Tito Reinhart <TKral@pdx.edu>
2022-11-15 16:38:47 -0800

    Added Docstring

--- src/access.py
+++ src/access.py
@@ -4,17 +4,26 @@ import json
 from pathlib import Path
 from api_lists import API_METHODS
 
-api_key = os.environ["NP_API_KEY"]
+def collectParkData():
+    '''
+        Function: Collects park data
+        Uses a data structure from api_lists.py
+        and iterates through API Call
+        to make api request using. 
+        These API Call Names are used 
+        to label the JSON File Output
+    '''
+    api_key = os.environ["NP_API_KEY"]
 
-for method in API_METHODS:
-    # Configure API request
-    pass_key = f'https://developer.nps.gov/api/v1/{method}?parkCode=or&api_key={api_key}'
-    response = urllib.request.urlopen(pass_key).read()
-    data = json.loads(response.decode('utf-8'))
-    if '/' in method:
-        name = method.replace('/','-')
-    else:
-        name = method
-    with open(f'../json_files/{name}_data.json','w') as fp:
-        for line in data['data']:
-            fp.write(json.dumps(line) + '\n')
+    for method in API_METHODS:
+        # Configure API request
+        end_point = f'https://developer.nps.gov/api/v1/{method}?parkCode=or&api_key={api_key}'
+        response = urllib.request.urlopen(end_point).read()
+        data = json.loads(response.decode('utf-8'))
+        if '/' in method:
+            name = method.replace('/','-')
+        else:
+            name = method
+        with open(f'../json_files/{name}_data.json','w') as fp:
+            for line in data['data']:
+                fp.write(json.dumps(line) + '\n')
