10c0aae11cc4828f37ef0863509bdb240853f3d1
Kira <kirak@pdx.edu>
2022-11-30 12:23:59 -0800

    Move the menu code into the Controller class methods, am not sure if this is the right placement yet.

--- src/main.py
+++ src/main.py
@@ -1,29 +1,11 @@
-from FileInterface import *
+
 from Controller import *
-from SendAlertsView import *
-from JsonModel import *
 
 def main():
     campsiteDataFile = "campsitesToMonitor.json"
-    #toFile = InterfaceToFile()
-    model = JsonModel(campsiteDataFile)
-    controller = Controller(model) #This isn't right, passing the model to the Controller...
-    #Use Type Hints in python to lie to the controller and tell it we are passing the interface class
-    
-    makeSelection = True
-    while(makeSelection):
-        selection = int(input("Type 1 to make a request, Type 2 to see all emails that need to receive alerts, Type 0 to quit: "))
-        if selection == 1:
-            mockPost = controller.getPostAsInput()
-            controller.sendRequestToModel(mockPost)
-        elif selection == 2:
-            sendAlerts = SendAlertsView(model)
-            sendAlerts.displayEmails()
-        elif selection == 0:
-            makeSelection = False
-        else:
-            print("That was not a valid selection, please try again.")
-    
+    controller = Controller(campsiteDataFile) 
+    controller.consoleMenu()
+
 
 if __name__ == "__main__":
     main()
