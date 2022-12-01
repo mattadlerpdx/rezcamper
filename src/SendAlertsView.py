eb47bd7d02fe4e2a5531d3de3756a5120d0b0c6b
matt <madler@pdx.edu>
2022-11-30 18:55:32 -0800

    email script works for console view

--- src/SendAlertsView.py
+++ src/SendAlertsView.py
@@ -1,6 +1,7 @@
 
 from FileInterface import *
-
+import smtplib
+from email.mime.text import MIMEText
 class SendAlertsView():
     def __init__(self, interface: InterfaceFromFile):
         self.interface = interface
@@ -12,5 +13,15 @@ class SendAlertsView():
     def displayEmails(self):
         self.getAlerts()
         print(f'These emails should receive an alert that their campsites are available: {self.alerts}')
+        sender = 'admin@example.com'
+        port = 1025
+        msg = MIMEText('testTEXT')
+        msg['Subject']= 'You ahve an opening'
+        msg['From'] = 'admin@example.com'
+        for reciever in self.alerts:
+            msg['To'] = reciever
+            with smtplib.SMTP('localhost',port) as server:
+                server.sendmail(sender,reciever,msg.as_string())
+                print('sent to: ', reciever) 
 
 
